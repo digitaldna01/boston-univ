14464573deb6f113e8cff35b1c64b626a5ecabe8
Jay Lee <jaylee@crc-dot1x-nat-10-239-12-166.bu.edu>
2024-01-25 23:46:24 -0500

    Add Fair coin simuation

--- CAS CS 365/HW/Study_Datascience.jl
+++ CAS CS 365/HW/Study_Datascience.jl
@@ -1,8 +1,16 @@
-
+## Import Julia Package
 using Random
 using Plots
 using StatsBase
 using DataFrames
 using Statistics
 
+## Toss a fair coin 1000 times and estimate the probability of observing heads using the fraction #heads/#tosses
+coin_sides = ["Tails","Heads"]
+n_tosses = 1000
+random_tosses = rand(coin_sides, n_tosses)
+probability_heads = count(==("Heads"), random_tosses) / n_tosses
+probability_tails = count(==("Tails"), random_tosses) / n_tosses
 
+println("Probability of Heads: ", probability_heads)
+println("Probability of Tails: ", probability_tails)
