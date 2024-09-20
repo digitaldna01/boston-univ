f39e0af5ce7535c334f0364dfa3e964afa4c45d7
Jay <jhonglee@bu.edu>
2024-09-19 23:42:22 -0400

    Finished CAS CS 350 HW 1

---
+++ CAS CS 350/HW/hw1/hw1/server.c
@@ -0,0 +1,178 @@
+/*******************************************************************************
+* Simple FIFO Order Server Implementation
+*
+* Description:
+*     A server implementation designed to process client requests in First In,
+*     First Out (FIFO) order. The server binds to the specified port number
+*     provided as a parameter upon launch.
+*
+* Usage:
+*     <build directory>/server <port_number>
+*
+* Parameters:
+*     port_number - The port number to bind the server to.
+*
+* Author:
+*     Renato Mancuso
+*
+* Affiliation:
+*     Boston University
+*
+* Creation Date:
+*     September 10, 2023
+*
+* Last Update:
+*     September 9, 2024
+*
+* Notes:
+*     Ensure to have proper permissions and available port before running the
+*     server. The server relies on a FIFO mechanism to handle requests, thus
+*     guaranteeing the order of processing. For debugging or more details, refer
+*     to the accompanying documentation and logs.
+*
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/* Include struct definitions and other libraries that need to be
+ * included by both client and server */
+#include "common.h"
+
+#define BACKLOG_COUNT 100
+#define USAGE_STRING				\
+	"Missing parameter. Exiting.\n"		\
+	"Usage: %s <port_number>\n"
+
+/* Main function to handle connection with the client. This function
+ * takes in input conn_socket and returns only when the connection
+ * with the client is interrupted. */
+static void handle_connection(int conn_socket)
+{
+	/* IMPLEMENT ME! */
+	// Define Parameter
+	int recv_message, send_message;
+	struct request req;
+	struct response res;
+	struct timespec receipt_timestamp, completion_timestamp;
+
+	while(1){
+		recv_message = recv(conn_socket, &req, sizeof(struct request), 0);
+
+		// stamp at which the server received the request
+		clock_gettime(CLOCK_MONOTONIC, &receipt_timestamp);
+
+		if(recv_message < 0) {
+			ERROR_INFO();
+			perror("Unable to read request");
+			close(conn_socket);
+			break;
+		}
+
+		// Check if clients closed the socket
+		if(recv_message == 0){
+			// The requested number of bytes to receive from a stream socket is 0
+			break;
+		}
+
+		//busy wait requested length
+		get_elapsed_busywait(req.length.tv_sec, req.length.tv_nsec);
+
+		res.id = req.id;
+		res.reserved = 0;
+		res.ack = 0;
+
+		send_message = send(conn_socket, &res, sizeof(struct response), 0);
+		if(send_message < 0){
+			ERROR_INFO();
+			perror("Unable to send response");
+			close(conn_socket);
+			break;
+		}
+
+		// stamp at which the server complete the request
+		clock_gettime(CLOCK_MONOTONIC, &completion_timestamp);
+
+		printf("R%lu:%f,%f,%f,%f\n", req.id, TSPEC_TO_DOUBLE(req.timestamp), TSPEC_TO_DOUBLE(req.length), TSPEC_TO_DOUBLE(receipt_timestamp), TSPEC_TO_DOUBLE(completion_timestamp));
+	}
+	
+}
+
+/* Template implementation of the main function for the FIFO
+ * server. The server must accept in input a command line parameter
+ * with the <port number> to bind the server to. */
+int main (int argc, char ** argv) {
+	int sockfd, retval, accepted, optval;
+	in_port_t socket_port;
+	struct sockaddr_in addr, client;
+	struct in_addr any_address;
+	socklen_t client_len;
+
+	/* Get port to bind our socket to */
+	if (argc > 1) {
+		socket_port = strtol(argv[1], NULL, 10);
+		printf("INFO: setting server port as: %d\n", socket_port);
+	} else {
+		ERROR_INFO();
+		fprintf(stderr, USAGE_STRING, argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	/* Now onward to create the right type of socket */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sockfd < 0) {
+		ERROR_INFO();
+		perror("Unable to create socket");
+		return EXIT_FAILURE;
+	}
+
+	/* Before moving forward, set socket to reuse address */
+	optval = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (void *)&optval, sizeof(optval));
+
+	/* Convert INADDR_ANY into network byte order */
+	any_address.s_addr = htonl(INADDR_ANY);
+
+	/* Time to bind the socket to the right port  */
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(socket_port);
+	addr.sin_addr = any_address;
+
+	/* Attempt to bind the socket with the given parameters */
+	retval = bind(sockfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in));
+
+	if (retval < 0) {
+		ERROR_INFO();
+		perror("Unable to bind socket");
+		return EXIT_FAILURE;
+	}
+
+	/* Let us now proceed to set the server to listen on the selected port */
+	retval = listen(sockfd, BACKLOG_COUNT);
+
+	if (retval < 0) {
+		ERROR_INFO();
+		perror("Unable to listen on socket");
+		return EXIT_FAILURE;
+	}
+
+	/* Ready to accept connections! */
+	printf("INFO: Waiting for incoming connection...\n");
+	client_len = sizeof(struct sockaddr_in);
+	accepted = accept(sockfd, (struct sockaddr *)&client, &client_len);
+
+	if (accepted == -1) {
+		ERROR_INFO();
+		perror("Unable to accept connections");
+		return EXIT_FAILURE;
+	}
+
+	/* Ready to handle the new connection with the client. */
+	handle_connection(accepted);
+
+	close(sockfd);
+	return EXIT_SUCCESS;
+
+}
