db8ad395ac77c5ed6e73b0b0574ca7a13354021b
digitaldna0q <ll.leejaehong.ll@gmail.com>
2025-02-25 20:45:38 -0500

    CS392

--- CAS CS 392/HW/HW5/b.py
+++ CAS CS 392/HW/HW5/b.py
@@ -1,5 +1,16 @@
-from itertools import combinations
-
 n, x = map(int, input().split())
 a = list(map(int, input().split()))
 
+count = 0
+window_sum = 0
+l = 0
+
+for r in range(n):
+    window_sum += a[r]
+    while ( window_sum > x and l <= r ):
+        window_sum -= a[l]
+        l += 1
+    if window_sum == x:
+        count += 1
+
+print(count)
